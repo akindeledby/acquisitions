27804625c25ee0549ac4ac5a7fd164ea68c3bdfa
Akindele Adebayo <akindeledby86@gmail.com>
2025-10-15 03:14:18 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import app from "./app.js"; // Here we specified the extension ".js" because we are working on a node app. This is not necessary in a React oor NextJS app
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`)
+})
